78ba800a156ec7485cc036683c30ac9c624e550e
Anton Smirnov <anton.smirnov.9910@gmail.com>
2021-08-04 02:32:02 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,152 @@
-# CIOM_deprating
+# Heroku deployment examples
+
+Workflow (works with public and privare repositories):
+
+- start with this template or modify your app taking this as an example
+- run `renv::init()` or `renv::snapshot()` to capture dependencies in the `renv.lock` file
+- work on your app
+- add secrets to your GitHub repo settings and follow steps below
+
+The `/app` folder contains the shiny app. The demo app was taken from here:
+https://github.com/analythium/shinyproxy-demo
+
+Note: Shiny apps [time out after 55 seconds](https://devcenter.heroku.com/articles/limits#http-timeouts):
+
+> After the initial response, each byte sent from the server restarts a rolling 55 second window. A similar 55 second window is restarted for every byte sent from the client.
+>
+> If no data is received from the dyno within the 55 second window the connection is terminated and an H15 error is logged.
+>
+> Similarly, if no data is received from the client within the 55 second window, the connection is terminated and an H28 error is logged.
+
+A workaround was posted on [SO](https://stackoverflow.com/questions/54594781/how-to-prevent-a-shiny-app-from-being-grayed-out) to print a dot to the console every passage of 10 seconds. 
+A counter set at 50 seconds interval is added to `/app/server.R`:
+
+```R
+## prevent timeout
+autoInvalidate <- reactiveTimer(intervalMs = 50*1000)
+observe({
+    autoInvalidate()
+    cat(".")
+})
+```
+
+The following deployment options are explained here:
+
+- [Using GitHub actions](#using-github-actions)
+- [Using local Heroku CLI](#using-local-heroku-cli)
+
+## Deployment using GitHub actions
+
+Log into Heroku, in the dashboard, click on 'New' then select 'Create new App'.
+Give a name (e.g. `shiny-cicd`, if available, this will create the app at https://shiny-cicd.herokuapp.com/) to the app and create the app.
+
+Got to the Settings tab of the repo, scrolld down to Secrets and add the
+following new repository secrets:
+
+- `HEROKU_EMAIL`: your Heroku email
+- `HEROKU_APP_NAME`: you application name from above
+- `HEROKU_API_KEY`: your Heroku api key, you can find it under your personal settings, click on reveal and copy
+
+See the `.github/workflows/deploy.yml` file for additional options
+(`dockerfile_name`, `docker_options`, `dockerfile_directory`)
+
+## Deployment using local Heroku CLI
+
+### Prerequisites
+
+- Install [Heroku CLI](https://devcenter.heroku.com/articles/heroku-cli#download-and-install)
+- R & Shiny
+- Docker Engine installed & a Docker Hub account
+
+### Building Docker Images with heroku.yml
+
+See docs: https://devcenter.heroku.com/articles/build-docker-images-heroku-yml
+
+#### Pieces
+
+Create a `heroku.yml` file in your application's root directory. The following example heroku.yml specifies the Docker image to build for the app’s web process:
+
+```yaml
+build:
+  docker:
+    web: Dockerfile
+```
+
+Heroku uses the `CMD` specified in the Dockerfile.
+
+The Dockerfile defines system requirements, R package dependencies.
+
+```Dockerfile
+FROM rocker/r-base:latest
+
+...
+
+ENV PORT=8080
+
+CMD ["R", "-e", "shiny::runApp('/home/app', host = '0.0.0.0', port=as.numeric(Sys.getenv('PORT')))"]
+```
+
+The port settings and the `CMD` part is different from ShinyProxy apps,
+port number is passed as an env var by the Heroku container runtime.
+
+#### Create app
+
+See also here: https://github.com/virtualstaticvoid/heroku-docker-r
+
+Commit your changes if any (only  repos with new commits will deploy once the app is already on Heroku):
+
+```git
+git commit -m "Changed"
+```
+
+Create the Heroku application with the container stack
+
+```bash
+heroku create --stack=container
+```
+
+Now you should see the app url.
+
+Or configure an existing application to use the container stack.
+
+```bash
+heroku stack:set container
+```
+
+#### Deploy
+
+Deploy your application to Heroku, replacing `<branch>` with your branch,
+e.g. `main`:
+
+```bash
+git push heroku <branch>
+```
+
+Now you see the build logs on the Heroku remote.
+
+#### Open app
+
+Now open the app in your browser:
+
+```bash
+heroku open
+```
+
+https://morning-springs-64281.herokuapp.com/
+
+### Test the Docker container locally
+
+To build the image from the Dockerfile, run:
+
+```bash
+sudo docker build -t analythium/heroku-demo .
+```
+
+Test:
+
+```bash
+docker run -p 4000:8080 analythium/heroku-demo
+```
+then visit `127.0.0.1:4000`.
+
+(c) Copyright [Analythium Solutions Inc.](https://analythium.io), 2021 (MIT).
---
+++ app/server.R
@@ -0,0 +1,148 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinyalert)
+library(googlesheets4)
+library(dplyr)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    linkMeta = "https://docs.google.com/spreadsheets/d/1gdbQJc36-6auD9IFuGAoBAf4T-cKIpEQM93q-TYCiQQ/edit?usp=sharing"
+    myData = read_sheet(linkMeta) %>% as.data.frame()
+    observeEvent(input$Speciality,{
+        withProgress(message = 'Rendering, please wait!', {
+            spec = isolate({input$Speciality})
+            course = isolate({input$Course})
+            link = myData[myData$Speciality == spec & myData$Course == course,"Link"]
+            if(is.na(link)){
+                tbl.Comp.Prod = NA
+            }
+            else{
+                tbl.Comp.Prod = read_sheet(link) %>% as.data.frame()
+            }
+            output$CompProd = renderDataTable({
+                validate(need(!is.na(tbl.Comp.Prod), "Нет данных"))
+                tbl.Comp.Prod
+            })
+        })
+
+        #output$Productivity = renderDataTable({
+        #    validate(need(is.list(tables), "Нет данных"))
+        #    tables$Productivity
+        #})
+    })
+    observeEvent(input$Course,{
+        withProgress(message = 'Rendering, please wait!', {
+            spec = isolate({input$Speciality})
+            course = isolate({input$Course})
+            link = myData[myData$Speciality == spec & myData$Course == course,"Link"]
+            if(is.na(link)){
+                tbl.Comp.Prod = NA
+            }
+            else{
+                tbl.Comp.Prod = read_sheet(link) %>% as.data.frame()
+            }
+            output$CompProd = renderDataTable({
+                validate(need(!is.na(tbl.Comp.Prod), "Нет данных"))
+                tbl.Comp.Prod
+            })
+        })
+
+        #output$Productivity = renderDataTable({
+        #    validate(need(is.list(tables), "Нет данных"))
+        #    tables$Productivity
+        #})
+    })
+    # output$CathedraRating = renderDataTable({
+    #     withProgress(message = 'Rendering, please wait!', {
+    #                     answers = isolate({myData()})
+    #                     t = calculateRating(answers)
+    #     })
+    #                     validate(need(nrow(t) > 0, "Нет данных"))
+    #                     t
+    #     })
+    #TODO - make error control and OnDiscipline
+    ## prevent timeout
+    autoInvalidate <- reactiveTimer(intervalMs = 50*1000)
+    observe({
+        autoInvalidate()
+        cat(".")
+    })
+})
+
+# calculateRating = function(inputTable){
+#
+#     require(binom)
+#
+#     #needTbl = inputTable
+#     needTbl = myData
+#     needTbl$Cathedra = as.factor(needTbl$Cathedra)
+#     departments = levels(needTbl$Cathedra)
+#
+#     out <- data.frame(matrix(ncol = 3, nrow = 0))
+#     colnames(out) = c("Место", "Название кафедры", "Рейтинг")
+#     problemsColumns = c("PO", "KP", "OUP", "MTO", "AOS")
+#     #weight coefficients
+#     wOUP = 1
+#     wKP = 0.4
+#     wPO = 0.7
+#     wAOS = 0.6
+#     wMTO = 0
+#
+#     i = 1
+#     for (d in departments) {
+#         dep = needTbl[needTbl$Cathedra == d,]
+#
+#         dep[,problemsColumns] = ifelse(dep[,problemsColumns] < 4,0,1)
+#         dep$WeightingRate = (wPO * dep$PO + wKP * dep$KP + wOUP * dep$OUP + wMTO * dep$MTO + wAOS * dep$AOS) / (wPO + wKP + wOUP + wMTO + wAOS)
+#         dep$Success = ifelse(dep$WeightingRate < 0.6,0,1)
+#         estimation = sum(dep$Success,na.rm = T)
+#         low_rate = round(binom.confint(estimation, nrow(dep),conf.level = 0.95, methods = "wilson")$lower,4)*100
+#         out[i,2] = d
+#         out[i,3] = low_rate
+#         i = i + 1
+#     }
+#
+#     out = out[order(out[,3],decreasing = T),]
+#     out[,1] = order(out[,3],decreasing = T)
+#     return(out)
+# }
+#
+# calculateDiscipline = function(inputTable,spec,course){
+#     inputTable = as.data.frame(inputTable)
+#     needTbl = inputTable[inputTable$Speciality == spec & inputTable$Course == course,]
+#     if(nrow(needTbl) == 0)
+#         return(-1)
+#     #TODO - weight coefficients is matrix. Answer in the Preparing column is number of the element in weight vector-row
+#     preparingWeights = data.frame(matrix(c(1,1,1,1,1),nrow = 1,byrow = T))
+#
+#
+#     needTbl$Subject = as.factor(needTbl$Subject)
+#     for (i in 1:nrow(needTbl)) {
+#         needTbl[i,"WeightComplication"] = needTbl[i,"Complication"] * preparingWeights[1, needTbl[i,"Preparing"]]
+#         needTbl[i,"WeightProductivity"] = needTbl[i,"Productivity"] * preparingWeights[1, needTbl[i,"Preparing"]]
+#     }
+#
+#     comp = data.frame(matrix(ncol = 2, nrow = 0))
+#     colnames(comp) <- c("Дисциплина", "Сложность")
+#     prod = data.frame(matrix(ncol = 2, nrow = 0))
+#     colnames(prod) <- c("Дисциплина", "Продуктивность")
+#
+#     i = 1
+#     for (s in levels(needTbl$Subject)) {
+#         t = subset.data.frame(needTbl,Subject == s)
+#         comp[i,1] = s
+#         prod[i,1] = s
+#         comp[i,2] = mean(t$WeightComplication)
+#         prod[i,2] = mean(t$WeightProductivity)
+#         i = i + 1
+#     }
+#     return(list(Complication = comp[order(comp[,2],decreasing = T),], Productivity = prod[order(prod[,2],decreasing = T),]))
+# }
